a03dac623cd713721a6a6782bb0e0292f125d84f
Jonathan Kee <48512757+jonathan-kee@users.noreply.github.com>
2019-10-06 15:49:19 +0800

    Add files via upload

---
+++ vueschool-forum/src/pages/PageHome.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="col-full">
+    <h1>Welcome to the Forum</h1>
+    <CategoryList :categories="categories"/>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data'
+import CategoryList from '@/components/CategoryList'
+console.log(sourceData)
+export default {
+  components: {
+    CategoryList
+  },
+  data () {
+    return {
+      categories: Object.values(sourceData.categories)
+    }
+  }
+}
+</script>
